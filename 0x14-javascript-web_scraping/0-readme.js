67a9e7290c841e7826accd497e59346f012d5fab
OtienoOdongo <otabiznake@gmail.com>
2023-07-25 13:10:05 +0000

    reading a file using fs

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 //a script that use fs module to read a file 
 
-const fs = require("fs"); //imports built-in nodejs file system (fs) module
+const fs = require('fs'); //imports built-in nodejs file system (fs) module
 
 //retrieving a filename from the commandline
 const file_name = process.argv[2]; //filename is the 3rd element of process.argv array
@@ -9,6 +9,6 @@ const file_name = process.argv[2]; //filename is the 3rd element of process.argv
 //reading the file asynchronously
 fs.readFile(file_name, (err, file_content) => {
 if (err) throw err;
-console.log(file_content.toString()); //toString() method is used to buffer object to string
+console.log(file_content.toString()); //toString() method is used to convert buffer object to string
 });
 
