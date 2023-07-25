1aa1c302aadf460c6208ee7e192f5f2f4ced64de
OtienoOdongo <otabiznake@gmail.com>
2023-07-25 13:03:31 +0000

    reading files using node.js built-in 'fs'

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -3,21 +3,12 @@
 
 const fs = require("fs"); //imports built-in nodejs file system (fs) module
 
-try {
-  const text = fs.readFileSync("cisfun", "utf-8");
-  console.log(text);
-} catch (err) {
-  if (err.code === "ENOENT") {
-    const errorObject = {
-      Error: err.message,
-      errno: err.errno,
-      code: err.code,
-      syscall: err.syscall,
-      path: err.path,
-    };
-    console.error(errorObject);
-  } else {
-    throw err;
-  }
-}
+//retrieving a filename from the commandline
+const file_name = process.argv[2]; //filename is the 3rd element of process.argv array
+
+//reading the file asynchronously
+fs.readFile(file_name, (err, file_content) => {
+if (err) throw err;
+console.log(file_content.toString()); //toString() method is used to buffer object to string
+});
 
