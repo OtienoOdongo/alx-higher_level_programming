eec7eb0badabc1ec742ef419f0a5d7d78f5495a2
OtienoOdongo <otabiznake@gmail.com>
2023-07-26 15:00:35 +0000

    script that gets webpage content and saves it

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -5,20 +5,8 @@ const fs = require('fs');
 
 const url = process.argv[2];
 const filename = process.argv[3];
-let options = { json: true };
-
-request.get(url, options, (error, response, body) => {
-  if (error) {
-    return console.error(err);
-  }
-
-  if (!error && response.statusCode === 200) {
-    try {
-      fs.writeFileSync(filename, JSON.stringify(body, null, 2), 'utf-8');
-      console.log(filename);
-    } catch (err) {
-      console.error(err);
-    }
+request.get(url, (error, response, body) => {
+  if (!error) {
+    fs.writeFileSync(filename, body, 'utf-8');
   }
 });
-
