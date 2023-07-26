3f8518ee1835b6c62526dbc709257e9f1d64ea47
OtienoOdongo <otabiznake@gmail.com>
2023-07-26 17:09:00 +0000

    script printing tasks completed

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -2,23 +2,24 @@
 
 const request = require('request');
 
-const completedtasks = {};
 const apiurl = process.argv[2];
+const results = {};
+
 request.get(apiurl, function (error, response, body) {
   if (!error) {
     const tasks = JSON.parse(body);
     for (const task of tasks) {
       const id = task.userId;
-      if (completedtasks[id] === undefined) {
-        completedtasks[id] = 0;
+      if (results[id] === undefined) {
+         results[id] = 0;
       }
       if (task.completed) {
-        completedtasks[id] += 1;
+        results[id] += 1;
       }
-      if (completedtasks[id] === 0) {
-        delete completedtasks[id];
+      if (results[id] === 0) {
+        delete results[id];
       }
     }
-    console.log(completedtasks);
+    console.log(results);
   }
 });
