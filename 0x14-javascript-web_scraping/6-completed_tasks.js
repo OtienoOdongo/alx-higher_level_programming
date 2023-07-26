374a77bb47a05ea3117e906ca79c73e43a03af10
OtienoOdongo <otabiznake@gmail.com>
2023-07-26 18:36:05 +0000

    script tha computes completed tasks

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,28 +4,21 @@ const request = require('request');
 
 const apiurl = process.argv[2];
 
-request.get(apiurl, (error, response, body) => {
+request.get(apiurl, { json: true }, (error, response, body) => {
   if (error) {
-    console.error(error);
-  } else if (response.statusCode !== 200) {
-    console.error(response.statusCode);
-  } else {
-    const todos = JSON.parse(body);
-    const result = {};
+    console.log(error);
+    return;
+  }
 
-    for (const todo of todos) {
-      const id = todo.userId;
-      if (result[id] === undefined) {
-        result[id] = 0;
-      }
-      if (todo.completed) {
-        result[id] += 1;
-      }
-      if (result[id] === 0) {
-        delete result[id];
+  const tasksCompleted = {};
+  body.forEach((todo) => {
+    if (todo.completed) {
+      if (!tasksCompleted[todo.userId]) {
+        tasksCompleted[todo.userId] = 1;
+      } else {
+        tasksCompleted[todo.userId] += 1;
       }
     }
-
-    console.log(result);
-  }
+  });
+  console.log(tasksCompleted);
 });
